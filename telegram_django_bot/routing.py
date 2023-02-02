73a646fc84232c914b43a363e205c8b8144fe81a
thepaket <101527506+thepaket@users.noreply.github.com>
2023-02-02 23:25:59 +0300

    Update routing.py

--- telegram_django_bot/routing.py
+++ telegram_django_bot/routing.py
@@ -96,10 +96,7 @@ class RouterCallbackMessageCommandHandler(Handler):
 
                 user = get_user_model().objects.filter(id=user_details.id).first()
                 if user:
-#                     try:
-#                         logging.info('user.current_utrl', user.current_utrl)
-#                     except Exception as e:
-#                         logging.error(e)
+                    logging.info('user.current_utrl', user.current_utrl)
                     if user.current_utrl:
                         callback_func = telega_resolve(user.current_utrl, self.utrl_conf)
         return callback_func
